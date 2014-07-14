cce76e98dda9f5a38bc710e1c4f15622f2636a26
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2014-07-14 13:32:18 +0900

    ファイルが自動でアップロードされるようになりました

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,15 @@ $(function() {
     console.log(fileInput);
   });
 });
+
+$(function() {
+  $('.directUpload').find("input:file").each(function(i, elem) {
+    var fileInput    = $(elem);
+    var form         = $(fileInput.parents('form:first'));
+    var submitButton = form.find('input[type="submit"]');
+    var progressBar  = $("<div class='bar'></div>");
+    var barContainer = $("<div class='progress'></div>").append(progressBar);
+    fileInput.after(barContainer);
+  });
+});
+
