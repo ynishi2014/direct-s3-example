47d8463619d0659e7ca10d803e3061bc25334cc5
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2014-07-14 10:39:41 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DirectS3Example::Application.config.secret_key_base = 'be792ddf8434cd263689509a0e83891482e3653c12756c77071baa04b6ab1d123cab5ac8236800b963bd53f807351d2b80599c4cd1874131567938158b83838d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DirectS3Example::Application.config.session_store :cookie_store, key: '_direct-s3-example_session'
